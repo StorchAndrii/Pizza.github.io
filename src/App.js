b8b4a2d8d4fc8fcb6273ffbf3d41fe679128d7e2
StorchAndrii <andrey.st.light@gmail.com>
2022-05-29 14:28:18 +0300

    add PizzaBlock

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import "./App.css";
+import "../src/scss/app.scss";
+import Header from "./components/Header/Header";
+import Categories from "./components/Categories/Categories";
+import Sort from "./components/Sort/Sort";
+import PizzaBlock from "./components/PizzaBlock/PizzaBlock";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title={"Чизбургер-пицца"} price={200} />
+            <PizzaBlock title={"Четыре-сыра"} price={300} />
+            <PizzaBlock title={"Мясная-пицца"} price={400} />
+            <PizzaBlock title={"Морепродукты"} price={500} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
