3c0f29b720323a7e4aa9f365b65bd3cfd6406dba
StorchAndrii <andrey.st.light@gmail.com>
2022-06-13 17:36:39 +0300

    add focus add debounce fix problem

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useCallback, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import debounce from "lodash.debounce";
+
 import search from "../../assets/img/search.svg";
 import clear from "../../assets/img/clear.svg";
 import styles from "./Search.module.scss";
-import { useDispatch, useSelector } from "react-redux";
 import { getPizza } from "../../redux/pizzaSlice/pizzaSlice";
 
 const Search = () => {
@@ -10,18 +12,40 @@ const Search = () => {
   const searchInputValue = useSelector(
     (state) => state.pizzaSlice.searchParams.search
   );
+  const [value, setValue] = useState("");
+
+  const updateSearchValue = useCallback(
+    debounce((str) => {
+      dispatch(getPizza({ search: str }));
+    }, 1000),
+    []
+  );
+
+  const inputRef = useRef();
+  const onClickClear = () => {
+    dispatch(getPizza({ search: "" }));
+    setValue("");
+    inputRef.current.focus();
+  };
+
+  const onChangeInput = (event) => {
+    setValue(event.target.value);
+    updateSearchValue(event.target.value);
+  };
+
   return (
     <div className={styles.root}>
       <img className={styles.icon} src={search} alt="search" />
       <input
         className={styles.input}
+        ref={inputRef}
         placeholder={"Поиск пиццы..."}
-        value={searchInputValue}
-        onChange={(event) => dispatch(getPizza({ search: event.target.value }))}
+        value={value}
+        onChange={onChangeInput}
       />
       {searchInputValue && (
         <img
-          onClick={() => dispatch(getPizza({ search: "" }))}
+          onClick={() => onClickClear()}
           className={styles.clear}
           src={clear}
           alt="clear"
